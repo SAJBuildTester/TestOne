26055f1d0f403f9b894e0d13a6c67134fdc567fd
Stephen Turton <stephen.turton@snagajob.com>
2014-11-03 15:49:32 -0500

    Initial Commit

---
+++ TestOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOne
+//
+//  Created by Stephen Turton on 11/3/14.
+//  Copyright (c) 2014 Stephen Turton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOne
+//
+//  Created by Stephen Turton on 11/3/14.
+//  Copyright (c) 2014 Stephen Turton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
